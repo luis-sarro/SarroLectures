87c43c4c8bf1d83de3571d68ede091975c18503d
luis-sarro <lsb@dia.uned.es>
2016-10-14 16:25:44 +0200

    reverse ICA components

--- install.r
+++ install.r
@@ -1,1 +1,4 @@
-install.packages(c("mvtnorm","dbscan","kohonen","fields","orclus","kernlab","scales","fields","knitr","FSelector","scales","rpart","graphics","ica","lle","rgl","diffusionMap"),dep=TRUE)
+
+# This assumes you have libssl-dev, libxml2-dev and oracle java (jdk) installed
+
+install.packages(c("plotrix","bnlearn","mvtnorm","dbscan","kohonen","fields","orclus","kernlab","scales","fields","knitr","FSelector","scales","rpart","ica","lle","rgl","diffusionMap"),dep=TRUE)
